a05e122b3bf004edde5185330ee2544e8f4392fe
irahulcse <1rahulchandra1@gmail.com>
2020-03-30 18:01:18 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,48 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'dart:async';
+void main() => runApp(MyApp());
 
-void main() => runApp(new MyApp());
-
-// class MyApp extends StatefulWidget {
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
-
-// class _MyAppState extends State<MyApp> {
-
-//   bool darkThemeEnabled=false;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: darkThemeEnabled?ThemeData.dark():ThemeData.light(),
-//       home:  HomePage(),
-//     );
-//   }
-
-//   Widget HomePage() {
-//     return Scaffold(
-//       appBar: new AppBar(
-//         title: new Text("Dynamic Theming in Flutter"),
-//       ),
-//       body: Center(
-//         child: Text("Hello World!"),
-//       ),
-//       drawer: new Drawer(
-//         child: new ListView(
-//           children: <Widget>[
-//             ListTile(
-//               title: Text("Dark Theme"),
-//               trailing: Switch(
-//                 value: darkThemeEnabled,
-//                 onChanged: (changedTheme){
-//                  setState(() {
-//                    darkThemeEnabled=changedTheme;
-//                  });
-//                 },
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-// }
-
+//Using Bloc
 class MyApp extends StatelessWidget {
-  //bool darkThemeEnabled = false;
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
       stream: bloc.darkThemeEnabled,
       initialData: false,
       builder: (context, snapshot) => MaterialApp(
-        theme: ThemeData.light(),
-        home: HomePage(snapshot.data),
+          theme: snapshot.data ? ThemeData.dark() : ThemeData.light(),
+          home: HomePage(snapshot.data)),
+    );
+  }
+  //
+}
+
+class HomePage extends StatelessWidget {
+  final bool darkThemeEnabled;
+
+  HomePage(this.darkThemeEnabled);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dynamic Theming In Flutter"),
+      ),
+      body: Center(
+        child: Text("Dartpad.dev"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text("Dark Theme"),
+              trailing: Switch(
+                value: darkThemeEnabled,
+                onChanged: bloc.changeTheme,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -73,34 +55,3 @@ class Bloc {
 }
 
 final bloc = Bloc();
-
-
-class HomePage extends StatelessWidget {
-  bool darkThemeEnabled;
-
-  HomePage(this.darkThemeEnabled);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-          appBar: AppBar(
-            title: Text("Dark Theme Implementation in Stateless Widget"),
-          ),
-          body: Center(
-            child: Text("Hello World"),
-          ),
-          drawer: new Drawer(
-            child: ListView(
-              children: <Widget>[
-                ListTile(
-                  title: Text("Dark Theme"),
-                  trailing: Switch(
-                    value: darkThemeEnabled,
-                    onChanged: bloc.changeTheme,
-                  ),
-                )
-              ],
-            ),
-          ),
-        );
-  }
-}
